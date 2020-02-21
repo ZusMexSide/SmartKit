4992470d8b7e6fa0d9e89bdc68acb3a41ef6b19f
jgcohetero <cohetero745@gmail.com>
2020-02-21 12:17:58 -0600

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+   path: 'login', component: LoginComponent 
+ },
   {
     path: 'temperatura',
     loadChildren: () => import('./temperatura/temperatura.module').then( m => m.TemperaturaPageModule)
